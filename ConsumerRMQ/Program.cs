7aee68377acc270a85ff63c0e0161a2029f9946c
shubham-yadav <sy7833446@gmail.com>
2024-10-02 12:14:46 +0530

    intial setup

--- ConsumerRMQ/Program.cs
+++ ConsumerRMQ/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using System.Threading.Channels;
+using RabbitMQ.Client;
+
+// connection to the RabbitMQ server 
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+var connection = factory.CreateConnection();
+
+// creating channel within the established RabbitMQ connection
+using (var channel = connection.CreateModel())
+{
+    for (int i = 0; i < 10000; i++)
+    {
+        // msg
+        string pubMsg = "I am shubham";
+
+        // convert it to byte code as RMQ communicate in byte
+        var byteMsg = Encoding.UTF8.GetBytes(pubMsg);
+
+        // now publish this msg 
+        channel.BasicPublish(
+            exchange: "ChatExchange",
+            routingKey: "PubSubKey",
+            basicProperties: null,
+            body: byteMsg
+        );
+
+        Console.WriteLine("Msg published > " + i + " " + pubMsg);
+    }
+
+}
--- ProducerRMQ/Program.cs
+++ ProducerRMQ/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using System.Threading.Channels;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+// connection to the RabbitMQ server 
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+var connection = factory.CreateConnection();
+
+//
+var channel = connection.CreateModel();
+
+// consumer 
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (m, arge) =>
+{
+    var receivedBody = arge.Body.ToArray();
+    var receivedMsg = Encoding.UTF8.GetString(receivedBody);
+    Console.WriteLine(receivedMsg);
+};
+
+channel.BasicConsume(queue: "ChatQueue", autoAck: true, consumer: consumer);
+
+Console.ReadLine();
